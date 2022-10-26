d5901b45cc1eb673e0c3f5eadc1599f1d876ac94
simicodes <simeonoffiong@gmail.com>
2022-10-26 14:31:58 +0100

    Business Component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
 
-const Button = () => {
+const Button: React.FC = () => {
   return (
-    <div>
-      Button
-    </div>
-  )
-}
+    <button
+      type="button"
+      className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none`}
+    >
+      Get Started
+    </button>
+  );
+};
 
-export default Button
+export default Button;
