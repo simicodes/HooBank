5dd8663246db38b7b91094eca3c1e1e43b767e40
simicodes <simeonoffiong@gmail.com>
2022-10-27 15:22:34 +0100

    Done with CardDeal component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,7 +4,7 @@ const Button: React.FC = () => {
   return (
     <button
       type="button"
-      className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none`}
+      className={`py-4 px-6 bg-blue-gradient rounded-[10px] font-poppins font-medium text-[18px] text-primary outline-none`}
     >
       Get Started
     </button>
